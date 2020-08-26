0fd4613b07857a08768f894cecf675746849eaab
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-26 09:13:13 -0600

    Adds and passes price

--- lib/house.rb
+++ lib/house.rb
@@ -1,9 +1,13 @@
 class House
+  attr_reader :address
 
   def initialize(price, address)
     @price   = price
     @address = address
-    
+
   end
 
+  def price
+    @price.delete("$").to_i
+  end
 end
--- test/house_test.rb
+++ test/house_test.rb
@@ -5,9 +5,10 @@ require './lib/house'
 
 class HouseTest < Minitest::Test
 
-  def test_it_exists
+  def test_it_exists_with_attributes
     house = House.new("$400000", "123 sugar lane")
 
     assert_instance_of House, house
+    assert_equal 400000, house.price
   end
 end
