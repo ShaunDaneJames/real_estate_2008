2beaf122ff32e8dac8c1b2cc8d5f3d44479d2ba9
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-26 09:17:02 -0600

    Adds and passes can add rooms

--- test/house_test.rb
+++ test/house_test.rb
@@ -13,9 +13,15 @@ class HouseTest < Minitest::Test
     assert_equal "123 sugar lane", house.address
   end
 
-  def test_it_starts_with_no_rooms
+  def test_it_starts_with_no_rooms_then_adds_them
     house = House.new("$400000", "123 sugar lane")
-
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
     assert_equal [], house.rooms
-  end 
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
+  end
 end
