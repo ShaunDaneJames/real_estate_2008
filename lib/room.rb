7e5fde564da9a7a40a8e4e1d6a78effe9bdc29b9
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-26 09:06:04 -0600

    Passes area test

--- lib/room.rb
+++ lib/room.rb
@@ -2,11 +2,14 @@ class Room
   attr_reader :category,
               :length,
               :width
-              
+
   def initialize(category, length, width)
     @category = category
     @length   = length
     @width    = width
   end
 
+  def area
+    @length * @width.to_i
+  end
 end
