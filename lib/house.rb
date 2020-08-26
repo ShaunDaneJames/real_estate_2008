be3a1a55fac664b965421a7f66edadc35496ee5e
Shaun James <ShaunDaneJames@Gmail.com>
2020-08-26 09:15:11 -0600

    Adds and passes starts with no rooms

--- lib/house.rb
+++ lib/house.rb
@@ -1,10 +1,11 @@
 class House
-  attr_reader :address
+  attr_reader :address,
+              :rooms
 
   def initialize(price, address)
     @price   = price
     @address = address
-
+    @rooms   = []
   end
 
   def price
